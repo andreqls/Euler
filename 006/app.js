f67ce51ec27f8fb1cbb9b91cf0323632e849d58b
Andre Silva <andreqls@gmail.com>
2020-06-13 13:50:29 -0300

    006: copied basic files

---
+++ 006/app.js
@@ -0,0 +1,14 @@
+var main = function () {
+    "use strict";
+
+    /* function call */
+    $("main").append(dummyFunc(0).toString())
+
+};
+
+$(document).ready(main);
+
+/* functions */
+var dummyFunc = function (input) {
+    return 0;
+};
