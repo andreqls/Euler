917d38aa14bf237a497e102905f8d8a4e04e9402
Andre Silva <andreqls@gmail.com>
2020-06-20 22:36:30 -0300

    050: Started working on isPrimePairSet

--- 060/app.js
+++ 060/app.js
@@ -12,3 +12,24 @@ $(document).ready(main);
 var dummyFunc = function (input) {
     return 0;
 };
+
+var isPrime = function (num) {
+	var d;
+	if (num==2) return true;
+	if (num<2 || num%2==0) return false;
+	for (d=3;(d*d)<=num;d+=2) {
+		if (num%d==0) return 0;
+	}
+	return true;
+}
+
+var isPrimePairSet = function (pset) {
+	var i,j;
+	for (i=0;i<pset.length;i++) {
+		for (j=0;j<pset.length;j++) {
+			if (i!=j) {
+
+			}
+		}
+	}
+};
