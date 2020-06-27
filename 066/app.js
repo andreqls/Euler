f014ed1965b1a88ac7e845265d17ccb6d34396e2
Andre Silva <andreqls@gmail.com>
2020-06-27 09:38:36 -0300

    066: Located wrong solutions, PRESUMABLY due to large integers involved

--- 066/app.js
+++ 066/app.js
@@ -27,32 +27,92 @@ var genPerfectSquares = function (limit) {
 	return ps;
 };
 
+var getXfromD = function (D) {
+	if (Math.sqrt(D)==Math.floor(Math.sqrt(D))) return 0;
+	var x,y,x2,y2,maxy=Math.sqrt(Math.pow(2,64)-1);
+	for (y=1;y<maxy;y++) {
+		y2=y*y;
+		x2=1+D*y2;
+		x=Math.sqrt(x2);
+		if (x==Math.floor(x)) return x;
+	}
+};
+
+var digDeepX = function (D) {
+	var y,x;
+	for (y=1;true;y++) {
+		x=Math.sqrt(1+D*y*y);
+		if (x==Math.floor(x)) return x
+	}
+};
+
+var getSolutions = function (limit) {
+	var D,maxD=0,maxX=0,solutions=[0];
+	for (D=1;D<=limit;D++) {
+		solutions[D]=getXfromD(D);
+		if (maxX<solutions[D]) { maxD=D; maxX=solutions[D]; }
+	}
+	console.log(maxD,maxX);
+	return solutions;
+};
+
+var checkSolutions = function(list) {
+	var D,x,y,mistakes=[];
+	for (D=1;D<list.length;D++) {
+		x=list[D];
+		if (x!=0) {
+			y=Math.sqrt((x*x-1)/D);
+			if (y!=Math.floor(y)) mistakes.push(D);
+		}
+	}
+	return mistakes;
+};
+
+var findAllSolutions = function (D) {
+}
+
+/*
+var getSolutions = function (limit) {
+	var i,next,D,solutions=[];
+	for (i=1;(i*i)<=limit;i=i++) {
+		solutions[i*i]=0;
+		next=(i+1)*(i+1);
+		for (D=(i*i)+1;D<((i+1)*(i+1));D++) {
+			solutions[D]=getXfromD(D);
+			console.log(D,solutions[D]);
+		}
+	}
+	return solutions;
+};*/
+
 var genXYDsets = function (limit) {
-	var max=0,Dmax=0,x2,y2,D,i,j,Dlist=[],pairs={},ps=genPerfectSquares(limit);
-	for (i=0;i<=1000;i++) Dlist[i]=0;
-	for (j=0;j<limit;j++) {
-		y2=ps[j];
-		for (i=j+1;i<limit;i++) {
-			x2=ps[i]; // x2 > y2
+	var max=0,Dmax=0,maxD=((limit+1)*limit/2),x,y,x2,y2,D,i,j,Dsum=0,pairs={}; // Dsum = Sum(1..1000) - Sum(1,4,9,16,...,961) = 
+	for (i=0;i<=Math.floor(Math.sqrt(limit));i++) maxD-=i*i; // set maxD to the Dsum target
+	console.log("maxD = ",maxD);
+	for (j=1;Dsum<maxD;j++) {
+		y2=j*j;
+		for (i=j+1;(i*i)<=(limit*y2+1);i++) {
+			x2=i*i; // x2 > y2
 			if ((x2-1)%y2==0) {
 				D=(x2-1)/y2;
-				if (D<=1000) {
-					x2=Math.sqrt(x2);
-					y2=Math.sqrt(y2);
-//					console.log(D.toString()+": "+x2.toString()+"² - "+D.toString()+"*"+y2.toString()+"² = 1");
-					if (pairs[D]==undefined || x2<pairs[D][0]) {
-						pairs[D]=[x2,y2];
-						Dlist[D]=1;
-						if (x2>max) {
-							Dmax=D;
-							max=x2;
-						}
+				if (D<=limit) {
+					x=Math.sqrt(x2);
+					y=Math.sqrt(y2);
+					if (pairs[D]==undefined) {
+						pairs[D]=[x,y];
+						Dsum+=D;
+						if (x>max) { Dmax=D; max=x; }
 					}
+/*					else if (x<pairs[D][0]) {
+						console.log("WOW!");
+						pairs[D]=[x,y];
+						if (x>max) { Dmax=D; max=x; } 
+					}*/
 				}
 			}
 		}
 	}
-	console.log(Dlist);
+//	console.log(Dsum);
 	console.log(Dmax,max);
 	return pairs;
 };
