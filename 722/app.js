d77142b68e9219ab4457f1789b75d76ab037649c
Andre Silva <andreqls@gmail.com>
2020-06-28 15:35:13 -0300

    722: Working but too slow to solve

--- 722/app.js
+++ 722/app.js
@@ -21,10 +21,30 @@ var sigma = function (k,n) {
 	return s;
 };
 
-var convergent = function (k,q,limit) {
-	var s,n,sum=0;
-	for (n=1;n<limit;n++) {
-		sum+=sigma(k,n)*Math.pow(q,n);
-	}
-	return sum;
+var sciNot = function (num) {
+	var exp;
+	for (exp=0;num>=10;exp++) num/=10;
+	return [num,exp];
 };
+
+var sigDif = function (a,b) {
+	return absVal(sciNot(a)[0]-sciNot(b)[0]);
+};
+
+var absVal = function (num) {
+	if (num<0) num*=-1;
+	return num;
+}
+
+var convergent = function (k,q) {
+	var s,n,prev,sum=0;
+	for (n=1;true;n++) {
+		prev=sum;
+		sum+=sigma(k,n)*Math.pow(q,n);
+		if (sigDif(sum,prev)<0.00000000000000001) { // converged to necessary precision
+			return sciNot(sum);
+		}
+	}
+//	return sum;
+};
+
